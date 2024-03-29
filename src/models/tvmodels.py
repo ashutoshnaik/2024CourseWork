1a85596fcda58cf20b7e2ea324d4400071d4f7a4
Ash Naik <ashutosh.naik@gmail.com>
2024-03-29 00:46:53 +0000

    Add debug

--- src/models/tvmodels.py
+++ src/models/tvmodels.py
@@ -11,6 +11,11 @@ class TorchVisionModel(nn.Module):
     def __init__(self, name, num_classes, loss, pretrained, **kwargs):
         super().__init__()
 
+	if name == "vit_b_16":
+    		print_hi("vit_b_16")
+	else:
+    		print_hi("Some other CNN")
+    
         self.loss = loss
         self.backbone = tvmodels.__dict__[name](pretrained=pretrained)
         self.feature_dim = self.backbone.classifier[0].in_features
